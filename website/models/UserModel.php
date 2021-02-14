99298fef87d50325944f01845d0de95cc557ae25
AzureSkyye <azureskyye59@gmail.com>
2021-02-14 16:04:52 -0400

    Update UserModel.php
    bug fix for the usermodl

--- website/models/UserModel.php
+++ website/models/UserModel.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('models/DatabaseModel.php');
+
 class UserModel extends Database
 {
 
@@ -9,11 +11,12 @@ class UserModel extends Database
             $conn = $this->connect_db();
 
             // Security against sql injections or malicious scripts
-            $userObj->setUsername(mysqli_real_escape_string($conn, $userObj->getUsername()));
+            $userObj->setFirstName(mysqli_real_escape_string($conn, $userObj->getFirstName()));
+            $userObj->setLastName(mysqli_real_escape_string($conn, $userObj->getLastName()));
             $userObj->setEmail(mysqli_real_escape_string($conn, $userObj->getEmail()));
-            $userObj->setPassword(mysqli_real_escape_string($conn, $userObj->getPassword()));
+            $userObj->setPkey(mysqli_real_escape_string($conn, $userObj->getPkey()));
             // Encrypting the password
-            $userObj->setPassword(sha1($userObj->getPassword()));
+            $userObj->setPkey(sha1($userObj->getPkey()));
 
             $sql = $conn->prepare("INSERT INTO users (first_name, last_name, email, pkey) VALUES(?, ?, ?, ?)");
             $sql->bind_param('ssss', $userObj->getFirstName(), $userObj->getLastName(), $userObj->getEmail(), $userObj->getPkey());
@@ -31,11 +34,11 @@ class UserModel extends Database
             $conn = $this->connect_db();
 
             // Security against sql injections or malicious scripts
-            $userObj->setUsername(mysqli_real_escape_string($conn, $userObj->getUsername()));
             $userObj->setEmail(mysqli_real_escape_string($conn, $userObj->getEmail()));
-            $userObj->setPassword(mysqli_real_escape_string($conn, $userObj->getPassword()));
-            // Encrypting the password
-            $userObj->setPassword(sha1($userObj->getPassword()));
+            $userObj->setPkey(mysqli_real_escape_string($conn, $userObj->getPkey()));
+
+            // Hashing the password
+            $userObj->setPkey(sha1($userObj->getPkey()));
 
             $sql = $conn->prepare("SELECT * FROM users WHERE email = ? AND pkey = ?");
             $sql->bind_param('ss', $userObj->getEmail(), $userObj->getPkey());
